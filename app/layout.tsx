1699a268971fb2c7d2b0d34c80584484920f942c
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2026-09-02 13:02:13 +0200

    Stop loading maze animation CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./slides.css";
 import { LanguageProvider } from "./context/LanguageContext";
 
 const geistSans = Geist({
